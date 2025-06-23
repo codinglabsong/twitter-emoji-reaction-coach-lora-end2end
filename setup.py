8981b3646792c4df88dd7b57879d2f189f527815
John Song <sjjjsong@gmail.com>
2025-06-23 16:04:10 -0700

    project restructuring

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="twitter_emoji_reaction_lora",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "transformers",
+        "datasets",
+        "peft",
+        "evaluate",
+        "python-dotenv",
+        "numpy",
+        "wandb",
+    ],
+)
